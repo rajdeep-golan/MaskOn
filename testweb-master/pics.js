07135b37759b56db665677d18fdec854d9b5fa5e
RAJDEEP SINGH GOLAN <38165635+rajdeep-golan@users.noreply.github.com>
2020-07-05 15:32:40 +0530

    Add files via upload

---
+++ testweb-master/pics.js
@@ -0,0 +1,7 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="js/simple-lightbox.min.js"></script>
+<script>
+$(function() {
+    const $gallery = $('.gallery a').simpleLightbox();
+   })
+</script>
